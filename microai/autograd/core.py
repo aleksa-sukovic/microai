56c791fb77ab65d2490087b47bc3500753f1ed13
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2023-11-22 19:21:28 +0100

    Adds exp, log and relu to autograd module

--- microai/autograd/core.py
+++ microai/autograd/core.py
@@ -1,3 +1,5 @@
+import math
+
 from typing import List, Union
 
 
@@ -7,10 +9,10 @@ class Variable:
         self._children = children
         self._value = value if value is not None else None
         self._grad = 0.0
+        self._forward() # ensure .data is always defined
 
     @property
     def data(self):
-        self._forward()
         return self._value
 
     @property
@@ -18,8 +20,7 @@ class Variable:
         return self._grad
 
     def backward(self, grad: float = None):
-        grad = 1.0 if grad is None else grad
-        self._backward(grad)
+        self._backward(1.0 if grad is None else grad)
 
     def _forward(self):
         pass
@@ -29,6 +30,15 @@ class Variable:
 
     def _to_var(self, other: float | int | None) -> "Variable":
         return Variable(other) if isinstance(other, int) or isinstance(other, float) else other
+
+    def exp(self) -> "Variable":
+        return ExpVariable(children=[self])
+
+    def log(self) -> "Variable":
+        return LogVariable(children=[self])
+
+    def relu(self) -> "Variable":
+        return ReLUVariable(children=[self])
 
     def __add__(self, other: "Variable") -> "Variable":
         return AddVariable(children=[self, self._to_var(other)])
@@ -100,5 +110,40 @@ class PowVariable(Variable):
     def _backward(self, grad: float):
         self._grad += grad
         base, exp = self._children[0].data, self._children[1].data
-        grad = grad * exp * base ** (exp - 1)
-        self._children[0]._backward(grad)
+        self._children[0]._backward(grad * exp * base ** (exp - 1))
+
+
+class ExpVariable(Variable):
+    def __init__(self, value: float | int = None, children: List[Variable] = []):
+        super().__init__(value, children, label="exp")
+
+    def _forward(self):
+        self._value = math.exp(self._children[0].data)
+
+    def _backward(self, grad: float):
+        self._grad += grad
+        self._children[0].backward(self.data * grad)
+
+
+class LogVariable(Variable):
+    def __init__(self, value: float | int = None, children: List[Variable] = []):
+        super().__init__(value, children, label="log")
+
+    def _forward(self):
+        self._value = math.log(self._children[0].data)
+
+    def _backward(self, grad: float):
+        self._grad += grad
+        self._children[0].backward((1 / self._children[0].data) * grad)
+
+
+class ReLUVariable(Variable):
+    def __init__(self, value: float | int = None, children: List[Variable] = []):
+        super().__init__(value, children, label="relu")
+
+    def _forward(self):
+        self._value = max(0, self._children[0].data)
+
+    def _backward(self, grad: float):
+        self._grad += grad
+        self._children[0].backward(int(self.data > 0) * grad)
